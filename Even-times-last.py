c1d6e5e0e6adede477fc769bd2eae459fd7eb805
jonathandavis <jondavis8128@gmail.com>
2021-04-23 01:49:16 -0400

    first

---
+++ Even-times-last.py
@@ -0,0 +1,10 @@
+# Given a sequence of integers, return the sum of all the integers
+# that have an even index, multiplied by the integer at the last index.
+# If the sequence is empty, you should return 0
+
+def even_last(numbers):
+    if not numbers:
+        return 0
+    else:
+        x = [val for key, val in enumerate(numbers) if key % 2 == 0]
+    return sum(x) * numbers[-1]
