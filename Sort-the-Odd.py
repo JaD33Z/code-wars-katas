fa9bb43e6616ed33201d2cfa93a01ea0be87a41e
JaD33Z <74392848+JaD33Z@users.noreply.github.com>
2021-07-24 23:06:13 -0400

    Update Sort-the-Odd.py

--- Sort-the-Odd.py
+++ Sort-the-Odd.py
@@ -1,13 +1,21 @@
 
+
+# ----------------------------------- Sort-the-Odd ---------------------------------- #
+
+
 # You will be given an array of numbers. You have to sort the
 # odd numbers in ascending order while leaving
 # the even numbers at their original positions.
 
-# [7, 1]  =>  [1, 7]
-# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
-# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+                                            # [7, 1]  =>  [1, 7]
+                                            # [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+                                            # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
 
 source_array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
 
 
 def sort_array(source_array):
@@ -15,7 +23,12 @@ def sort_array(source_array):
     return [next(odds) if i % 2 else i for i in source_array]
 
 
+
 print(sort_array(source_array))
 
+
 # output:
-# [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+            # [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+    
+    
+    
